1d62483a73382047fb397781da539c339cd3cca2
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2021-12-14 19:37:04 -0500

    added no students at campus message

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -2,6 +2,24 @@ import { Link } from "react-router-dom";
 
 const CampusView = (props) => {
   const {campus, deleteCampus} = props;
+  
+  if (!campus.students.length) {
+    return (
+    <div>
+      <h1>{campus.name}</h1>
+      <p>{campus.description}</p>
+      <h3>There are no students at this campus.</h3>
+      <Link to={`/campuses`}>
+      <button onClick={() => deleteCampus(campus.id)}>Delete</button>
+      </Link>
+      <br/>
+      <br/>
+      <Link to={`/campuses`}><button>Back to All Campuses</button></Link>
+      <br/>
+      <Link to={`/`}><button>Back to Home</button></Link>
+    </div>
+    )
+  }
   return (
     <div>      
       <h1>{campus.name}</h1>
