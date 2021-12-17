c6bb4c899ae2ff5478323e7c693a60c8a77b67d3
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2021-12-17 18:12:28 -0500

    fixed student img

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -16,7 +16,7 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <img src={student.imageUrl} alt="Student profile image"/>
+      <img src={student.imageUrl} alt="Student profile image" className={classes.image}/>
       <br></br>
       <p>{student.email}</p>
       <p>GPA: {student.gpa}</p>
